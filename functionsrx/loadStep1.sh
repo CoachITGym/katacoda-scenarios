0d53224f6a5b2201f4f6b162762a6c7a978a3b2d
Vedu Hariths <dev@theitgym.com>
2021-03-20 20:22:52 -0400

    fixed directory

--- functionsrx/loadStep1.sh
+++ functionsrx/loadStep1.sh
@@ -1,12 +1,12 @@
 echo "Started"
 
-cat << 'EOF' > /root/step1/multiply.js
+cat << 'EOF' > /root/multiply.js
 function multiply(a, b){
   a * b
 }
 EOF
 
-cat << 'EOF' > /opt/step1/test.js
+cat << 'EOF' > /opt/test.js
 var x = multiply(2, 2);
 if(x === 4) {
   process.exit(0);
